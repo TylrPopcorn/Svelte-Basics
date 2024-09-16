92c897a64b97c36b9467fa4f74a281f4293d2d3b
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-09-16 09:12:58 -0400

    Inline Event Handlers

---
+++ src/Inline-Event-Handlers.svelte
@@ -0,0 +1,61 @@
+<!-- https://svelte.dev/docs/introduction -->
+
+<!-- JavaScript -->
+<script>
+    //variable
+	let people = [
+        {name: 'Yoshi', Color: "Red", num: 23, id: 1},
+        {name: 'Luigi', Color: "Orange", num: 14, id: 2},
+        {name: 'Mario', Color: "Pink", num: 50, id: 3},
+    ];
+
+    //function
+    const handleClick = (id) => {
+        //delete person from people
+        people = people.filter((person)=>{
+            person.id != id
+        })
+    }
+</script>
+
+<!-- HTML -->
+<main>
+	<h1>Hello World</h1>
+
+    <!-- LOOP -->
+    {#each people as person (person.id)}
+         <!-- show data -->
+        <div>
+            <h3>{person.name}</h3>
+            <p>{person.Color}</p>
+            <!-- inline event -->
+            <button on:click={()=>{handleClick(person.id)}}>Delete</button>
+        </div>
+    {:else}
+    <!-- If array is empty, this will show-->
+        <p>No people found</p>
+    {/each}
+</main>
+
+<!-- CSS -->
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
